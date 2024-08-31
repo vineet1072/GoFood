2a1f545b98988e9898783d58d858520c7ad26050
vineet1072 <90325407+vineet1072@users.noreply.github.com>
2024-08-31 23:53:10 +0530

    Update Home.jsx

--- client/src/screen/Home.jsx
+++ client/src/screen/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
   const loadData = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/foodData", {
+      const response = await fetch("https://gofood-server-ih85.onrender.com/api/foodData", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
