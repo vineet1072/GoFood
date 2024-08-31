d4028da49850ef77396bf6a52d44e378488658a1
vineet1072 <90325407+vineet1072@users.noreply.github.com>
2024-08-31 23:55:51 +0530

    Update SignUp.jsx

--- client/src/screen/SignUp.jsx
+++ client/src/screen/SignUp.jsx
@@ -14,7 +14,7 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://gofood-server-ih85.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
