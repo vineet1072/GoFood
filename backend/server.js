9e43a6737b22439ec33e229f9fbe69534165288c
vineet1072 <vineetmhatre54@gmail.com>
2024-11-25 17:20:36 +0530

    third  commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 const connectDb = require("./utils/db.js");
