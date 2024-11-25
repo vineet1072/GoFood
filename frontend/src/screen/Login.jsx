918dad91b85c066bb3e1d21f9b8d5bd4732cb994
vineet1072 <vineetmhatre54@gmail.com>
2024-11-25 18:01:10 +0530

    commit

--- frontend/src/screen/Login.jsx
+++ frontend/src/screen/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("https://gofood-893l.onrender.com/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- frontend/src/screen/SignUp.jsx
+++ frontend/src/screen/SignUp.jsx
@@ -14,18 +14,21 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/createuser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: credentials.name,
-        email: credentials.email,
-        password: credentials.password,
-        location: credentials.geolocation,
-      }),
-    });
+    const response = await fetch(
+      "https://gofood-893l.onrender.com/api/createuser",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: credentials.name,
+          email: credentials.email,
+          password: credentials.password,
+          location: credentials.geolocation,
+        }),
+      }
+    );
 
     const json = await response.json();
     console.log(json);
